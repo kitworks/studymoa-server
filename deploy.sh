30c0df09b1532be78de368bd2d49a3f61da0653c
김천규 <redtree941@naver.com>
2022-05-24 09:40:08 +0900

    .

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,6 @@ echo "$blue"
 blue_app=`docker-compose -p studymoa-server ps  | grep Up | grep '4000->4000' | awk '{print $1}'`
 if [ $blue -eq 1 ]; then
 	echo "\$VALUE is 1"
-    # docker-compose up -d 
     start_app="green"
     down_app="blue"
     echo "2" > config.txt
@@ -21,26 +20,23 @@ else
     down_app="green"
     echo "1" > config.txt
     echo "CURRENT=blue" > .env
-    # docker-compose up -d 
 fi
 source .env
 echo "이미지 빌드"
 docker-compose build
 # sleep 10
 echo "start_app == $start_app down_app == $down_app"
-docker-compose up -d $start_app
-echo "아파치 리로드"
-echo $CURRENT
-# docker-compose exec -e CURRENT=${CURRENT} web bash
 echo "s/$down_app/$start_app/g"
 sed -i .back "s/$down_app/$start_app/g" ./studymoa_web/000-default.conf
-# sed -i studymoa_web/000-default.conf `s/$down_app/$start_app/g` studymoa_web/000-default.conf
-docker-compose exec -e CURRENT=${CURRENT} web service apache2 reload
-# docker-compose exec -e CURRENT=${CURRENT} web service apache2 reload
-# docker-compose exec -e CURRENT=${CURRENT} web service apache2 reload
+is_run_web=`docker-compose -p studymoa-server ps  | grep Up | grep 'web' | wc -l`
+if [ $is_run_web -eq 1 ]; then
+    docker-compose up -d $start_app
+    docker-compose exec -e CURRENT=${CURRENT} web service apache2 reload
+else 
+    docker-compose up -d web $start_app
+fi
 
 docker stop $down_app > /dev/null
 # sleep 10
 # docker-compose up -d web
-# cat config.txt
 docker-compose ps
