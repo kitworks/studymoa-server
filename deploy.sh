97a1673de2494b41477098ef21b1b89b67169908
김천규 <redtree941@naver.com>
2022-05-24 13:31:50 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -37,6 +37,7 @@ is_run_web=`docker-compose -p studymoa-server ps  | grep Up | grep 'web' | wc -l
 log "아파치 컨테이너 체크 == $is_run_web"
 if [ $is_run_web -eq 1 ]; then
     docker-compose up -d $start_app
+    sleep 10
     docker-compose exec -e CURRENT=${CURRENT} web service apache2 reload
     log "아파치 리로드"
 else 
